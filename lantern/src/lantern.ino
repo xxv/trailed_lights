eef5c3354f2103c8e54a07d1bd73048a282ad3fe
Steve Pomeroy <steve@staticfree.info>
2017-04-02 21:21:17 -0400

    First pass at lantern ESP8266 device

---
+++ lantern/src/lantern.ino
@@ -0,0 +1,124 @@
+#include "Secrets.h"
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#include <FastLED.h>
+#include <Ticker.h>
+#include <PubSubClient.h>
+
+enum DeviceMode {
+  booting = 0,
+  normal,
+  wifi_setup
+};
+
+#define MOTION_PIN 13
+#define NUM_LEDS 1
+// milliseconds
+#define RETRIGGER_DELAY 15000
+
+WiFiManager wifiManager;
+Ticker animationTicker;
+
+WiFiClientSecure wifi;
+PubSubClient client(wifi);
+
+CRGB leds[NUM_LEDS];
+uint8_t mac[WL_MAC_ADDR_LENGTH];
+char device_id[9];
+char lantern_id_color[23];
+char lantern_id_motion[24];
+char lantern_id_status[24];
+char color_hex[7];
+
+DeviceMode device_mode = booting;
+
+long last_motion = 0;
+
+void configModeCallback(WiFiManager *myWiFiManager) {
+  device_mode = wifi_setup;
+  FastLED.showColor(CRGB::Yellow);
+}
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  if (strcmp(lantern_id_color, topic) == 0) {
+    leds[0] = strtoul((char*)payload, nullptr, 16);
+    FastLED.show();
+  }
+}
+
+void setup() {
+  pinMode(0, OUTPUT);
+  pinMode(MOTION_PIN, INPUT);
+
+  Serial.begin(115200);
+
+  FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
+  FastLED.showColor(CRGB::Black);
+
+  Serial.println("Connecting...");
+  wifiManager.setAPCallback(configModeCallback);
+
+  if (!wifiManager.autoConnect("Lantern")) {
+    ESP.reset();
+    delay(1000);
+  }
+
+  WiFi.macAddress(mac);
+  sprintf(device_id, "%02x%02x%02x%02x", mac[2], mac[3], mac[4], mac[5]);
+  sprintf(lantern_id_color, "lantern/%s/color", device_id);
+  sprintf(lantern_id_motion, "lantern/%s/motion", device_id);
+  sprintf(lantern_id_status, "lantern/%s/status", device_id);
+
+  randomSeed(micros());
+  Serial.println("Wifi Connected");
+
+  FastLED.showColor(CRGB::Black);
+  client.setServer(mqtt_host, mqtt_port);
+  client.setCallback(mqtt_callback);
+}
+
+void check_motion() {
+  long now = millis();
+  if (digitalRead(MOTION_PIN) && (last_motion == 0 || (now - last_motion) > RETRIGGER_DELAY)) {
+    last_motion = now;
+    client.publish(mqtt_topic, "");
+    delay(500);
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Create a random client ID
+    String clientId = "lantern-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(lantern_id_status, "online");
+      client.subscribe(lantern_id_color);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void loop() {
+  while (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop();
+  check_motion();
+}
